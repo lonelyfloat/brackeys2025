6f5ff93363bdd2cfaf076eaf9cee8df22f42f82b
Fin <fastsmith3@gmail.com>
2025-02-16 13:13:13 -0500

    test

--- cam.gd
+++ cam.gd
@@ -1,13 +1,12 @@
 extends Camera2D
 
 @onready var player = $"../Player"
-@export var speed = 5
-@export var max_dist = 10
+@export var speed = 3
 
 func _ready() -> void:
 	position_smoothing_enabled = true
 	position_smoothing_speed = speed
 
 func _physics_process(delta: float) -> void:
-	position.x = move_toward(position.x, player.position.x, speed)
-	position.y = move_toward(position.y, player.position.y, speed)
+	position.x = player.position.x
+	position.y = player.position.y
