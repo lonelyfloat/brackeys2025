234d3bf832ed2dc485c4c206ea0c934b469f6c2e
Fin <fastsmith3@gmail.com>
2025-02-16 13:56:59 -0500

    a

--- player.gd
+++ player.gd
@@ -2,13 +2,15 @@ extends CharacterBody2D
 
 @export var speed = 300
 var speed_nerft = 1
+var bounce = false
+var vel
 
 func _physics_process(delta: float) -> void:
 
 	var horz_direction := Input.get_axis("ui_left", "ui_right")
 	var vert_direction := Input.get_axis("ui_up", "ui_down")
 	
-	if horz_direction || vert_direction:
+	if (horz_direction || vert_direction) && !bounce:
 		if abs(horz_direction) > .5 && abs(vert_direction) > .5:
 			speed_nerft = 0.66
 		else:
@@ -16,6 +18,23 @@ func _physics_process(delta: float) -> void:
 		velocity.y = vert_direction * speed * speed_nerft
 		velocity.x = horz_direction * speed * speed_nerft
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.y = move_toward(velocity.y, 0, speed)
-	move_and_slide()
+		velocity.x = move_toward(velocity.x, 0, speed/30)
+		velocity.y = move_toward(velocity.y, 0, speed/30)
+		
+	if bounce:
+		velocity += vel;
+		velocity.x = move_toward(velocity.x, 0, speed/1.2)
+		velocity.y = move_toward(velocity.y, 0, speed/1.2)
+		
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		if fmod(collision.get_angle(),PI) > 0.77:
+			vel = Vector2(velocity.x * -1,velocity.y)
+		else:
+			vel = Vector2(velocity.x,velocity.y * -1)
+		bounce = true
+		bounce_time(0.22)
+		
+func bounce_time(seconds: float) -> void:
+	await get_tree().create_timer(seconds).timeout
+	bounce = false
