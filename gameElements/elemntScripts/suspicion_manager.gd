a96e4e8692f0aad4be7eaa57c9f2201c2068b73f
Fin <fastsmith3@gmail.com>
2025-02-21 22:01:55 -0500

    Fix tile colliders

--- gameElements/elemntScripts/suspicion_manager.gd
+++ gameElements/elemntScripts/suspicion_manager.gd
@@ -2,13 +2,22 @@ extends Node2D
 
 var npcs: Array[Node]
 var suspicion_level := 0.0
+var sus_spoken := -1.0
 
 func _ready(): 
-    npcs = get_tree().get_nodes_in_group("NPCs")
-    for npc in npcs: 
-        npc.suspicion_raised.connect(_suspicion_raised_effect)
+	npcs = get_tree().get_nodes_in_group("NPCs")
+	for npc in npcs: 
+		npc.suspicion_raised.connect(_suspicion_raised_effect)
 
 func _suspicion_raised_effect(amount):
-    suspicion_level += amount
-    pass # to be implemented
-    
+	suspicion_level += amount
+	pass # to be implemented
+	
+func _process(delta: float) -> void:
+	match suspicion_level:
+		0.0 when sus_spoken < 0:
+			print("We are number one!")
+			sus_spoken = 0
+		2.0 when sus_spoken < 2:
+			print("Two are better than one!")
+			sus_spoken = 2
