566a318cb43780afea3e289562854974b67d4175
Fin <fastsmith3@gmail.com>
2025-02-21 22:54:53 -0500

    textpopups

--- gameElements/elemntScripts/suspicion_manager.gd
+++ gameElements/elemntScripts/suspicion_manager.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+@export var text : RichTextLabel
+@export var info_UI : Node
+
+
 var npcs: Array[Node]
 var suspicion_level := 0.0
 var sus_spoken := -1.0
@@ -16,8 +20,15 @@ func _suspicion_raised_effect(amount):
 func _process(delta: float) -> void:
 	match suspicion_level:
 		0.0 when sus_spoken < 0:
-			print("We are number one!")
-			sus_spoken = 0
+			info_UI.visible = true
+			text.text = "OK agent 46, the mission is simple. All you need to do is locate the target, Jimmy Red Legs, and take him out with a well placed bullet. Just press 'E' to ready your weapon - but becareful it will also make you more suspicous. Then take aim with the mouse and 'left click' to fire. Good luck agent, I know you take care of your first mission with ease."
+			if(Input.is_action_just_released("accept")):
+				info_UI.visible = false
+				sus_spoken = 0
+				
 		2.0 when sus_spoken < 2:
-			print("Two are better than one!")
-			sus_spoken = 2
+			info_UI.visible = true
+			text.text = "this is a second mission promt"
+			if(Input.is_action_just_released("accept")):
+				info_UI.visible = false
+				sus_spoken = 2
