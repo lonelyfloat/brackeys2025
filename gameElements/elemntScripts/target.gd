0982e3f6f1bd78c361daa24633fcd6f1a978fac8
Fin <fastsmith3@gmail.com>
2025-02-21 20:17:57 -0500

    RoomsDone

--- gameElements/elemntScripts/target.gd
+++ gameElements/elemntScripts/target.gd
@@ -7,6 +7,6 @@ signal target_dead()
 var dead := false
 
 func _process(_delta: float) -> void:
-    if !Engine.is_editor_hint() && !dead && self.health <= 0:
-        target_dead.emit()
-        dead = true
+	if !Engine.is_editor_hint() && !dead && self.health <= 0:
+		target_dead.emit()
+		dead = true
