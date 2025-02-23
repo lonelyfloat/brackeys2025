867612a098b83f292ac17343047ae400c534ed89
lonelyfloat <cephhead_prsnl@protonmail.com>
2025-02-22 22:55:37 -0500

    dongus

--- gameElements/elemntScripts/dead_scene.gd
+++ gameElements/elemntScripts/dead_scene.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 func _on_quit_button_down() -> void:
-	get_tree().quit()
+    get_tree().change_scene_to_file("res://main_menu.tscn")
 
 
 func _on_play_button_down() -> void:
-	get_tree().change_scene_to_file("res://basicTestScene.tscn")
+    get_tree().change_scene_to_file("res://basicTestScene.tscn")
